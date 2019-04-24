b050bb30e447405e1623d34bf6c983ab41f50a2f
dipliveweb <dipliveweb@gmail.com>
2019-04-23 20:54:55 -0400

    LiquidStudio Api integration + Updated File upload features

--- src/app/ask-question/ask-question.component.ts
+++ src/app/ask-question/ask-question.component.ts
@@ -1,33 +1,31 @@
-import { Component, OnInit,Input } from '@angular/core';
-import{FileUploadComponent} from '../file-upload/file-upload.component';
-import{AskQuestionService, Message} from '../shared/services/ask-question.service';
-
-import {Observable, BehaviorSubject} from "rxjs";
-import { scan } from 'rxjs/operators'
+import { Component, OnInit, Input } from "@angular/core";
+import { FileUploadComponent } from "../file-upload/file-upload.component";
+import {AskQuestionService, Message} from "../shared/services/ask-question.service";
+import { Observable, BehaviorSubject } from "rxjs";
+import { scan } from "rxjs/operators";
 
 @Component({
-  selector: 'app-ask-question',
-  templateUrl: './ask-question.component.html',
-  styleUrls: ['./ask-question.component.css']
+  selector: "app-ask-question",
+  templateUrl: "./ask-question.component.html",
+  styleUrls: ["./ask-question.component.css"]
 })
 export class AskQuestionComponent implements OnInit {
-    
-    messages: Observable<Message[]>;
-    formValue: string;
-    
-  constructor(private askQuestion: AskQuestionService) { }
+
+  messages: Observable<Message[]>;
+  formValue: string;
+  data: any = [];
+
+  constructor(private askQuestion: AskQuestionService) {}
 
   ngOnInit() {
-      //this.askQuestion.talk();
-    this.messages = this.askQuestion.conversation.asObservable()
-      .pipe(
-        scan((acc, val) => acc.concat(val))
-      )
-  }
-    
-  sendMessage(){
-      this.askQuestion.converse(this.formValue);
-      this.formValue = '';
+    this.messages = this.askQuestion.conversation
+      .asObservable()
+      .pipe(scan((acc, val) => acc.concat(val)));
   }
 
+  sendMessage() {
+    //console.log("Fetching from Local+ inside of ask-ques comp" +sessionStorage.getItem("fileContent"));
+    this.askQuestion.converse(this.formValue,sessionStorage.getItem("fileContent"));
+    this.formValue = "";
+  }
 }
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,42 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import {FileUploadService} from '../shared/services/file-upload.service';
-import {Router} from '@angular/router';
-
+import { Component, AfterViewInit, OnInit } from "@angular/core";
+import { FileUploadService } from "../shared/services/file-upload.service";
+import { Router } from "@angular/router";
+import { s3 } from "fine-uploader/lib/core/s3";
+import * as AWS from "aws-sdk";
 
 @Component({
-  selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+  selector: "app-file-upload",
+  templateUrl: "./file-upload.component.html",
+  styleUrls: ["./file-upload.component.css"]
 })
-
 export class FileUploadComponent implements OnInit {
-  constructor( private uploadFile: FileUploadService,private router: Router ) { }
+  constructor(private uploadFile: FileUploadService, private router: Router) {}
 
- selectedFile: File;
- public name : string ="";
-    
-  oneFileChanged(event: any):void {
-    this.selectedFile = event.target.files[0].name;
+  //selectedFile: File;
+  fileText: string;
+  public name: string = "";
+
+  oneFileChanged(event: any): void {
+    //this.selectedFile = event.target.files[0].name;
+    let reader: any = new FileReader();
+    reader.readAsText(event.target.files[0]);
+
+    reader.onload = function() {
+      this.fileText = reader.result.toString();
+      console.log("Inside of File-Upload page" +this.fileText);
+      sessionStorage.setItem("fileContent", this.fileText);
+    };
+
     //this.selectedFile = event.target.files[1].name;
     //this.selectedFile = event.target.files[2].name;
     //this.selectedFile = event.target.files[3].name;
     //this.selectedFile = event.target.files[4].name;
-   console.log(this.selectedFile);
-  }
-    
-  onUpload():void{
-    
-    //service code goese here
-    /*  this.uploadFile.uploadFileToServer(this.selectedFile)
-      .subscribe(event => {
-      console.log("inside of component" + event); // handle event here
-    });*/ 
 
-    console.log(this.name);
-    this.router.navigate(['./ask-question']);
-  }    
-    
-  ngOnInit() {
-  }
+    // let isDone: boolean = true;
+    // const AWSService = AWS;
+    // const region = "<insert your region here>";
+    // const bucketName = "<insert your bucket name>";
+    // const IdentityPoolId = "<insert your identity pool id>";
+    // const file = event.target.files[0];
 
+    // //Configures the AWS service and initial authorization
+    // AWSService.config.update({
+    //   region: region,
+    //   credentials: new AWSService.CognitoIdentityCredentials({
+    //     IdentityPoolId: IdentityPoolId
+    //   })
+    // });
+
+    // //adds the S3 service, make sure the api version and bucket are correct
+    // const s3 = new AWSService.S3({
+    //   apiVersion: "2006-03-01",
+    //   params: { Bucket: bucketName }
+    // });
+
+    // s3.upload(
+    //   { Key: file.name, Bucket: bucketName, Body: file, ACL: "public-read" },
+    //   function(err, data) {
+    //     if (err) {
+    //       console.log(err, "there was an error uploading your file");
+    //       isDone = false;
+    //     }
+    //   }
+    // );
+
+    // console.log("Check isDone" + isDone);
+    // if (isDone) {
+    //   this.router.navigate(["./ask-question"]);
+    // }
+
+    this.router.navigate(["./ask-question"]);
+  }
+  ngOnInit() {}
 }
